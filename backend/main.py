8c58ad08e502f22d5fce0ea8a6501caca410171f
Meow <seansean988@gmail.com>
2025-06-12 19:44:46 +0800

    add CORS

--- backend/main.py
+++ backend/main.py
@@ -4,9 +4,29 @@ from auth.routes import router as auth_router
 from dotenv import load_dotenv
 import os
 from db.database import init_db
+from fastapi.middleware.cors import CORSMiddleware
+
+
 load_dotenv()
 os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+
+
+allowed_origins = os.getenv("ALLOWED_ORIGINS", "").split(",")
+
+
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth_router)
 app.include_router(api_router)
+
+
 init_db()
